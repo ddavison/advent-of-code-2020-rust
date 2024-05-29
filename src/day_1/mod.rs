f957ef8dda92fb49cda9b810a2c55f4fa470d211
ddavison <daniel.jj.davison@gmail.com>
2024-05-29 12:13:00 -0400

    Day 1 Part 2

--- src/day_1/mod.rs
+++ src/day_1/mod.rs
@@ -1,4 +1,4 @@
-use crate::{Part, read_lines};
+use crate::{Part, Part::One, Part::Two, read_lines};
 
 #[cfg(test)]
 mod tests;
@@ -11,7 +11,7 @@ const SUM: i32 = 2020;
 /// [Advent of Code](https://adventofcode.com/2020/day/1)
 pub(crate) fn day_1(part: Part, input_file: &str) -> i32 {
     let mut expenses: Vec<i32> = Vec::new();
-    let mut factors: (i32, i32) = (0, 0);
+    let mut factors: Vec<i32> = Vec::new();
 
     if let Ok(lines) = read_lines(input_file) {
         for line in lines.map_while(Result::ok) {
@@ -20,18 +20,29 @@ pub(crate) fn day_1(part: Part, input_file: &str) -> i32 {
     }
 
     for idx in 0..expenses.len() {
-        match scan(expenses[idx], &expenses[idx + 1..]) {
-            Some((a, b)) => factors = (a, b),
+        match scan(expenses[idx], &part, &expenses[idx + 1..]) {
+            Some(v) => v.iter().for_each(|f| factors.push(*f)),
             None => continue
         }
     }
 
-    factors.0 * factors.1
+    factors.iter().product()
 }
 
-fn scan(start: i32, numbers: &[i32]) -> Option<(i32, i32)> {
-    for number in numbers {
-        if start + *number == SUM { return Some((start, *number)) }
+fn scan(start: i32, part: &Part, numbers: &[i32]) -> Option<Vec<i32>> {
+    match part {
+        One => {
+            for number in numbers {
+                if start + *number == SUM { return Some(vec![start, *number]) }
+            }
+        },
+        Two => {
+            for first in numbers {
+                for second in &numbers[1..] {
+                    if start + first + second == SUM { return Some(vec![start, *first, *second]) }
+                }
+            }
+        }
     }
 
     None
--- src/day_1/tests.rs
+++ src/day_1/tests.rs
@@ -4,7 +4,9 @@ use crate::{day_1, Part};
 fn day_1() {
     // Part 1
     assert_eq!(day_1::day_1(Part::One, "src/day_1/data/example.txt"), 514579);
-    assert_eq!(day_1::day_1(Part::One, "src/day_1/data/input.txt"), 980499)
+    assert_eq!(day_1::day_1(Part::One, "src/day_1/data/input.txt"), 980499);
 
     // Part 2
+    assert_eq!(day_1::day_1(Part::Two, "src/day_1/data/example.txt"), 241861950);
+    assert_eq!(day_1::day_1(Part::Two, "src/day_1/data/input.txt"), 200637446);
 }
